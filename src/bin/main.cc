c6ca930e8bb19642c54ddb52f3c78c60225b28cc
Tyler Rockwood <rockwood@openai.com>
2024-05-19 02:53:13 +0000

    Add landlock sandbox wrapper script
    Signed-off-by: Tyler Rockwood <rockwood@openai.com>

--- src/bin/main.cc
+++ src/bin/main.cc
@@ -1,6 +1,13 @@
 #include <fmt/core.h>
 
+#include "lib/add.h"
+#include "lib/mul.h"
+#include "lib/slow_mul.h"
+
 int main() {
-  fmt::print("Hello World!\n");
+
+  fmt::println("1 + 2 = {}", myproject::add(1, 2));
+  fmt::println("4 * 3 = {}", myproject::multiply(4, 3));
+  fmt::println("4 * 3 = {}", myproject::slow_multiply(4, 3));
   return 0;
 }
